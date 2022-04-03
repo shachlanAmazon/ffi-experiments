3bb055c5007bf3e699413e8c712c0e353ef886ff
Shachar Langbeheim <shachlan@amazon.com>
2022-04-03 21:01:13 +0300

    Simple ffi.

---
+++ Csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public static class main {
+  [DllImport("rust_ffi", CallingConvention = CallingConvention.Cdecl, EntryPoint="get_val")]
+  public static extern int GetValue();
+
+  public static void Main() {
+    Console.WriteLine(GetValue());
+    Console.WriteLine("done");
+  }
+}
