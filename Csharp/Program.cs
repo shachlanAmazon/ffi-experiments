8cf4889efff257590b49f1b86cd80d7894dc4ad9
Shachar Langbeheim <shachlan@amazon.com>
2022-04-03 22:26:22 +0300

    ffi with allocation

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-public static class main {
-  [DllImport("rust_ffi", CallingConvention = CallingConvention.Cdecl, EntryPoint="get_val")]
+public static class main
+{
+  [DllImport("rust_ffi", CallingConvention = CallingConvention.Cdecl, EntryPoint = "get_val")]
   public static extern int GetValue();
 
-  public static void Main() {
+  [DllImport("rust_ffi", CallingConvention = CallingConvention.Cdecl, EntryPoint = "concat_string")]
+  public static extern string ConcatString(string str);
+
+  [DllImport("rust_ffi", CallingConvention = CallingConvention.Cdecl, EntryPoint = "count_string")]
+  public static extern ulong CountString(string str);
+
+  [DllImport("rust_ffi", CallingConvention = CallingConvention.Cdecl, EntryPoint = "get_string")]
+  public static extern string GetString();
+
+  public static void Main()
+  {
+    Console.WriteLine(GetString());
+    Console.WriteLine(CountString("so again"));
     Console.WriteLine(GetValue());
+    Console.WriteLine(ConcatString("foobar"));
     Console.WriteLine("done");
   }
 }
--- rust-ffi/src/lib.rs
+++ rust-ffi/src/lib.rs
@@ -1,4 +1,46 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
+
 #[no_mangle]
 pub extern "C" fn get_val() -> i32 {
-        42
+    print!("check");
+    42
+}
+
+#[no_mangle]
+pub extern "C" fn count_string(input: *mut c_char) -> usize {
+    unsafe {
+        let cstr = CString::from_raw(input);
+        let vec = cstr.as_bytes();
+        let length = vec.len();
+        std::mem::forget(cstr);
+        length
+    }   
+}
+
+#[no_mangle]
+pub extern "C" fn get_string() -> *const c_char {
+    let str = CString::new("foobar").expect("hoo ha");
+    let p = str.as_ptr();
+    std::mem::forget(str);
+    p
+}
+
+#[no_mangle]
+pub extern "C" fn concat_string(input: *mut c_char) -> *const c_char {
+    unsafe {
+        print!("bef");
+        let cstr = CString::from_raw(input);
+        print!("first");
+        let str = cstr.into_string().expect("ti worked?");
+        print!("sec");
+        let concat = format!("{str}{str}");
+        print!("3rd");
+        let result = CString::new(concat).expect("second new failed");
+        print!("4th");
+        let p = result.as_ptr();
+        std::mem::forget(str);
+        std::mem::forget(result);
+        p
     }
+}
